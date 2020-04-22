9264d5026ac65ce67cda7bfd1d3f084b7731e589
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2020-04-22 05:53:10 +0200

    WIP for MelonLoader

--- DeveloperConsole/DeveloperConsole.cs
+++ DeveloperConsole/DeveloperConsole.cs
@@ -1,87 +1,82 @@
 ﻿using UnityEngine;
 using Scene = UnityEngine.SceneManagement;
+using MelonLoader;
 
-namespace DeveloperConsole
-{
+namespace DeveloperConsole {
 
-    internal class DeveloperConsole
-    {
+    internal class DeveloperConsole : MelonMod {
 
-        public static void OnLoad()
-        {
-            Object.Instantiate(Resources.Load("uConsole"));
-            uConsole.m_Instance.m_Activate = KeyCode.F1;
-            AddConsoleCommands();
+        private bool init = false;
+        private static bool flyEnabled = false;
+
+        public override void OnUpdate() {
+            if (!init) {
+                MelonModLogger.Log("Update");
+                Object.Instantiate(Resources.Load("uConsole"));
+                uConsole.m_Instance.m_Activate = KeyCode.F1;
+                AddConsoleCommands();
+                init = true;
+                MelonModLogger.Log("Done!");
+            }
         }
 
-        private static void AddConsoleCommands()
-        {
-            uConsole.RegisterCommand("load_scene", LoadScene);
+        private static void AddConsoleCommands() {
+            uConsole.RegisterCommand("load_scene", new System.Action(LoadScene));
 
-            uConsole.RegisterCommand("fly", Fly);
+            uConsole.RegisterCommand("fly", new System.Action(Fly));
 
-            uConsole.RegisterCommand("save", () => GameManager.m_PendingSave = true);
+            uConsole.RegisterCommand("save", new System.Action(() => GameManager.m_PendingSave = true));
 
-            uConsole.RegisterCommand("scene_name", () => Debug.Log(Scene.SceneManager.GetActiveScene().name));
+            uConsole.RegisterCommand("scene_name", new System.Action(() => Debug.Log((Il2CppSystem.String) Scene.SceneManager.GetActiveScene().name)));
 
-            uConsole.RegisterCommand("scene_index", () => Debug.Log(Scene.SceneManager.GetActiveScene().buildIndex));
+            uConsole.RegisterCommand("scene_index", new System.Action(() => Debug.Log((Il2CppSystem.String) Scene.SceneManager.GetActiveScene().handle.ToString())));
 
-            uConsole.RegisterCommand("pos", GetPosition);
+            uConsole.RegisterCommand("pos", new System.Action(GetPosition));
 
-            uConsole.RegisterCommand("tp", Teleport);
+            uConsole.RegisterCommand("tp", new System.Action(Teleport));
         }
 
-        private static void LoadScene()
-        {
+        private static void LoadScene() {
             var ind = uConsole.GetInt();
             SceneManager.LoadScene(ind);
         }
 
-        private static void Fly()
-        {
-            bool fly = !FlyMode.m_Enabled;
+        private static void Fly() {
+            bool fly = !flyEnabled;
             if (uConsole.GetNumParameters() > 0 && uConsole.NextParameterIsBool())
                 fly = uConsole.GetBool();
-            if (fly == FlyMode.m_Enabled)
-                return;
-            if (fly)
-                FlyMode.Enter();
-            else
-                FlyMode.TeleportPlayerAndExit();
-        }
-
-        private static void GetPosition()
-        {
-            Vector3 pos = GameManager.GetVpFPSPlayer().transform.position;
-            Debug.LogFormat("[{0:F2} / {1:F2} / {2:F2}]", pos.x, pos.y, pos.z);
-        }
-
-        private static void Teleport()
-        {
-            Vector3 target;
-
-            if (uConsole.GetNumParameters() < 2)
-            {
-                Debug.Log("Usage: tp x z    or    tp x y z.\nExample: tp 123 890");
+            if (fly == flyEnabled) {
                 return;
             }
-            else if (uConsole.GetNumParameters() == 2)
-            {
+            if (fly) {
+                FlyMode.Enter();
+            } else {
+                FlyMode.TeleportPlayerAndExit();
+            }
+        }
+
+        private static void GetPosition() {
+            Vector3 pos = GameManager.GetVpFPSPlayer().transform.position;
+            Debug.Log((Il2CppSystem.String) string.Format("[{0:F2} / {1:F2} / {2:F2}]", pos.x, pos.y, pos.z));
+        }
+
+        private static void Teleport() {
+            Vector3 target;
+
+            if (uConsole.GetNumParameters() < 2) {
+                Debug.Log((Il2CppSystem.String) "Usage: tp x z    or    tp x y z.\nExample: tp 123 890");
+                return;
+            } else if (uConsole.GetNumParameters() == 2) {
                 float x = uConsole.GetFloat();
                 float z = uConsole.GetFloat();
 
                 Vector3 start = new Vector3(x, 10000f, z);
-                if (Physics.Raycast(start, Vector3.down, out RaycastHit raycastHit, float.PositiveInfinity, Utils.m_PhysicalCollisionLayerMask | 1048576 | 134217728))
-                {
+                if (Physics.Raycast(start, Vector3.down, out RaycastHit raycastHit, float.PositiveInfinity, Utils.m_PhysicalCollisionLayerMask | 1048576 | 134217728)) {
                     target = raycastHit.point + new Vector3(0, 0.01f, 0);
-                }
-                else
-                {
+                } else {
                     target = new Vector3(x, 0, z);
                 }
-            }
-            else
-            {
+            } else {
                 float x = uConsole.GetFloat();
                 float y = uConsole.GetFloat();
                 float z = uConsole.GetFloat();
